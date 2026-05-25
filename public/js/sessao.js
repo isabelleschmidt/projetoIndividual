c9d499233ed1354a12f9f9527113e8dcdea70e8a
isabelleschmidt <isabelle.schmidt@sptech.school>
2026-05-25 11:52:00 -0300

    correção de erros de redirecionamento

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,5 +1,5 @@
 // sessão
-/*function validarSessao() {
+function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
@@ -14,18 +14,4 @@
 function limparSessao() {
     sessionStorage.clear();
     window.location = "../login.html";
-}
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
 }
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,9 +1,9 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+
 
 function autenticar(req, res) {
-    var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+    var email = req.params.email;
+    var senha = req.params.senha;
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
@@ -17,26 +17,12 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
-                    } else if (resultadoAutenticar.length == 0) {
+                    if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
+                    } else if (resultadoAutenticar.length > 1) {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    } else {
+                        res.json(resultadoAutenticar)
                     }
                 }
             ).catch(
